3616c38375e8271cb8e8965e7541e00a9d7047ee
assadribhi <assadribhi@gmail.com>
2020-06-15 18:07:06 +0300

    Assad's Hobby

--- story.js
+++ story.js
@@ -1,7 +1,12 @@
 // Define the name here on line 2
-
+let name = "Assad";
 // Define the age here on line 4
-
+let age = 28;
 // Define the hobby here on line 6
-
+let hoppy = "Code";
 // Print the story here on line 8
+console.log(
+  `Mr. ${name}, in ${
+    58 - age
+  } years, you will be 58 years. By then you'll have decade of ${hoppy} Practice!`
+);
